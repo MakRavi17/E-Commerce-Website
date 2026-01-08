927f8806cc69f4097c2e883a9d79b0cc41938eb5
MakRavi17 <makwanaravi1710@gmail.com>
2026-01-08 16:06:57 +0530

    Updated App and Contact components, added login CSS

--- src/App.jsx
+++ src/App.jsx
@@ -6,7 +6,7 @@ import Home from "./Components/Home";
 import Contact from "./Components/Contact";
 import Privacy from "./Components/Privacy";
 import Signup from "./Components/Signup";
-// import Loginform from "./Components/Loginform";
+import Loginform from "./Components/Loginform";
 
 import "./App.css";
 
@@ -19,7 +19,7 @@ const App = () => {
         <Route path="/privacy" element={<Privacy />} />
         <Route path="/contact" element={<Contact />} />
         <Route path="/signup" element={<Signup />} />
-        {/* <Route path="/loginform" element={<Loginform />} /> */}
+        <Route path="/loginform" element={<Loginform />} />
       </Routes>
     </>
   );
